25c74b18812c84ac1320ed58c804763a5721092a
araquach <adam@contra-design.com>
2020-12-12 07:45:10 +0000

    Got things set up

--- main.go
+++ main.go
@@ -1,148 +1,81 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
 func main() {
-	split3()
+	getStockOut()
+	getStockList()
 }
 
-func split1() {
-	file := "stock.csv"
+func getStockOut() {
+	file := "data/stock-out.csv"
 
-	data, err := ioutil.ReadFile(file)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	csvFile, _ := os.Open(file)
 
-	section := strings.SplitAfter(string(data), "Brand ")
+	r := csv.NewReader(bufio.NewReader(csvFile))
 
-	fmt.Println(section[4])
+	fmt.Println("-- Stock Out --")
 
-	// var combined []string
-
-	for _, split := range section[2:] {
-		p := strings.Split(split, "\n")
-		lineSplit := strings.Split(p[0], " -")
-
-		supplier := lineSplit[0]
-
-		cat := strings.TrimLeft(lineSplit[1], "Category ")
-		cat = strings.TrimRight(cat, ",")
-
-		fmt.Printf("Supplier: %v\nCategory: %v\n\n", supplier, cat)
-	}
-}
-
-func split2() {
-	var err error
-	var files []string
-
-	root := "data"
-
-	err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
 		}
-		if filepath.Ext(path) != ".csv" {
-			return nil
-		}
-		files = append(files, path)
-		return nil
-	})
-	if err != nil {
-		log.Println(err)
-	}
-	for _, fileName := range files {
-		fname := strings.Split(fileName, "/")[1]
-
-		fileBytes, err := ioutil.ReadFile(fname)
 		if err != nil {
 			log.Println(err)
 		}
+		product := record[0]
+		quantity := record[1]
 
-		split := strings.SplitAfter(string(fileBytes), "\n")
-
-		var section []string
-
-		for _, line := range split {
-			if !strings.Contains(line, "Page") {
-				section = append(section, line)
-			}
-		}
-
-		joined := strings.Join(section, "")
-
-		for _, l := range joined {
-
-			s := strings.SplitAfter(string(l), "\n")
-
-			csvReady := strings.Join(s, "")
-
-			r := csv.NewReader(strings.NewReader(csvReady))
-
-			for {
-				record, err := r.Read()
-				if err == io.EOF {
-					break
-				}
-				if err != nil {
-					log.Println(err)
-				}
-				fmt.Println(record)
-			}
+		if len(record[1]) > 0 && !strings.Contains(record[0], "Page") &&
+			!strings.Contains(record[1], "#") &&
+			!strings.Contains(record[0], "Total"){
+			fmt.Printf("%v - Quantity Used: %v\n", product, quantity)
 		}
 	}
 }
 
-func split3() {
-	file := "stock.csv"
+func getStockList() {
+	file := "data/stock-list.csv"
 
-	fmt.Println(file)
+	csvFile, _ := os.Open(file)
 
-	fileBytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		log.Println(err)
-	}
+	r := csv.NewReader(bufio.NewReader(csvFile))
 
-	sections := strings.SplitAfter(string(fileBytes), "Brand ")
+	fmt.Println("-- Stock List --")
 
-	for _, s := range sections[2:] {
-		split := strings.SplitAfter(s, "\n")
-
-		lineSplit := strings.Split(split[0], " -")
-
-		supplier := lineSplit[0]
-
-		csvData := split[12:len(split)-1]
-
-		csvReady := strings.Join(csvData, "")
-
-		r := csv.NewReader(strings.NewReader(csvReady))
-
-		fmt.Printf("Supplier: %v\n", supplier)
-
-		for {
-			record, err := r.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Println(err)
-			}
-			product := record[0]
-			quantity := record[4]
-
-			fmt.Printf("%v - Quantity Sold: %v\n", product, quantity)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
 		}
+		if err != nil {
+			log.Println(err)
+		}
+		product := record[0]
+		supplier := record[1]
+		brand := record[2]
+		cat := record[3]
+		sub := record[4]
+		retail := record[5]
+		if retail == "1" {
+			retail = "Yes"
+		} else {
+			retail = "No"
+		}
+
+		fmt.Printf("%v - %v - %v - %v - %v - %v\n", product, supplier, brand, cat, sub, retail)
 	}
+}
+
+func combine() {
+
 }
