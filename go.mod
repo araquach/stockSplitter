e4b64e86c9286817d33ba95beaba8829a1d2dc94
Adam Carter <adam@contra-design.com>
2020-12-12 08:53:13 +0000

    Got it working

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/araquach/stockSplitter
 
 go 1.14
+
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -7,12 +7,50 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 )
 
+type StockData struct {
+	ID int `json:"id" gorm:"primary_key"`
+	Product string `json:"product"`
+	Quantity int `json:"quantity"`
+	Price int `json:"price"`
+	Supplier string `json:"supplier"`
+	Brand string `json:"brand"`
+	Category string `json:"category"`
+	SubBrand string `json:"sub_brand"`
+	Retail string `json:"retail"`
+}
+
+func dbConn() (db *gorm.DB) {
+	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
-	getStockOut()
-	getStockList()
+	db := dbConn()
+	db.LogMode(true)
+	db.AutoMigrate(&StockData{})
+	db.Close()
+
+	//getStockOut()
+	//getStockList()
+
+	combine()
 }
 
 func getStockOut() {
@@ -77,5 +115,61 @@ func getStockList() {
 }
 
 func combine() {
+	var stockTransfers []StockData
+	var err error
 
+	so := "data/stock-out.csv"
+
+	csvFile, _ := os.Open(so)
+
+	sod := csv.NewReader(bufio.NewReader(csvFile))
+
+	for {
+		sor, err := sod.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Println(err)
+		}
+
+		if len(sor[1]) > 0 && !strings.Contains(sor[0], "Page") &&
+			!strings.Contains(sor[1], "#") &&
+			!strings.Contains(sor[0], "Total"){
+		}
+
+		sl :="data/stock-list.csv"
+
+		csvFile2, _ := os.Open(sl)
+
+		sld := csv.NewReader(bufio.NewReader(csvFile2))
+
+		for {
+			slr, err := sld.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Println(err)
+			}
+
+			if slr[0] == sor[0] {
+
+				quantity, _ := strconv.Atoi(sor[1])
+				price, _ := strconv.Atoi(sor[2])
+
+				stockTransfers = append(stockTransfers, StockData{Product: sor[0], Quantity: quantity, Price: price, Supplier: slr[1], Brand: slr[2], Category: slr[3], SubBrand: slr[4], Retail: slr[5]})
+
+			}
+		}
+	}
+	for _, r := range stockTransfers {
+		db := dbConn()
+		db.LogMode(true)
+		db.Create(&r)
+		if err != nil {
+			log.Println(err)
+		}
+		db.Close()
+	}
 }
